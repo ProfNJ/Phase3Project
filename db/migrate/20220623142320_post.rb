b6e5f33ebf1d2be80295bd83ecae435e67609107
Rory <rorygran5@gmail.com>
2022-06-23 10:37:04 -0400

    fixed migrations

---
+++ db/migrate/20220623142320_post.rb
@@ -0,0 +1,9 @@
+class Post < ActiveRecord::Migration[6.1]
+  def change
+    create_table :posts do |t|
+      t.string :author
+      t.integer :author_id
+      t.string :date
+      t.string :content
+  end
+end

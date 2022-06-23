8673e6a55a89b36b1cd7be504f76c7a72ddbf6ca
Rory <rorygran5@gmail.com>
2022-06-23 01:04:27 -0400

    backend skeleton

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,4 @@
-class User < ActiveRecord::Base
+class Comment < ActiveRecord::Base
     has_many :users
     has_many :posts, through: :users
   end
---
+++ db/migrate/create_post.rb
@@ -0,0 +1,14 @@
+class CreatePosts < ActiveRecord::Migration[6.1]
+    def change
+      create_table :posts do |t|
+        t.string :author
+        t.integer :author_id
+        t.string :date
+        t.string :content
+        
+        
+  
+      end
+    end
+  end
+  
